78c13db07e2072eef6980610d694edc88ed5e0d3
Eriks <eriks@e-osis.local>
2024-09-19 14:37:05 +0300

    Initial commit

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
+const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 
-// Replace 'YOUR_TELEGRAM_BOT_TOKEN' with the token you got from the BotFather
-const token = '7221990979:AAF8foSmrHHPYHsCuL0J1poduf7RJBIS4y4';
-
-// Create a bot that uses 'polling' to fetch new updates
+const token = 'YOUR_TELEGRAM_BOT_TOKEN';
 const bot = new TelegramBot(token, { polling: true });
 
-// Listen for any kind of message. There are different kinds of messages.
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
+// Express app to listen to $PORT
+const app = express();
+const port = process.env.PORT || 3000; // Use the port assigned by Heroku or default to 3000
 
-    // Check if the message is a command and if it's the /start command
-    if (msg.text.toLowerCase() === '/start') {
-        // Send an image with a caption and a button
-        bot.sendPhoto(chatId, './photo_2024-08-22 11.23.28.jpeg', {
-            caption: '♠️Welcome to Card Captain👨🏻‍✈️ - 🤖AI Blackjack card counting mini-app♥️\nTo start the mini-app, press the button below 👇',
-            reply_markup: {
-                inline_keyboard: [[
-                    { text: '♥️Open App♠️', web_app: {url: 'https://cardcaptain.netlify.app' }}
-                ]]
-            }
-        });
-    }
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  if (msg.text.toLowerCase() === '/start') {
+    bot.sendPhoto(chatId, './photo_2024-08-22 11.23.28.jpeg', {
+      caption: '♠️Welcome to Card Captain👨🏻‍✈️ - 🤖AI Blackjack card counting mini-app♥️\nTo start the mini-app, press the button below 👇',
+      reply_markup: {
+        inline_keyboard: [[
+          { text: '♥️Open App♠️', web_app: { url: 'https://cardcaptain.netlify.app' } }]
+        ]
+      }
+    });
+  }
 });
